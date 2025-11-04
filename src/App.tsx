062e7166057b30a18e5a5b1caa079f638837f908
shreeteja22 <smutukundu2206@gmail.com>
2025-11-04 09:37:11 +0530

    feat: vercel analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <div className="min-h-screen bg-(--color-background)">
       <Navbar />
       <Home />
+      <Analytics />
     </div>
   );
 }
